f1167fdb03b2556aa6a387a0b904f0e0b132a338
kazuho0531 <kazuho.053187@gmail.com>
2020-03-22 17:40:05 +0900

    targeterルーティングをネストする

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
-  resources :groups
-  resources :targeters
-
+  resources :groups do
+    resources :targeters
+  end
 
 end
 
