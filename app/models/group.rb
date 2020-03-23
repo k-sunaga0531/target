5b2cfc8339a8c9e85b0b7ca22c7fdd94b2e77735
kazuho0531 <kazuho.053187@gmail.com>
2020-03-23 10:19:31 +0900

    サイドバーにグループ名とメンバー表示

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,14 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :targeters
   validates :group_name, presence: true, uniqueness: true
+
+
+  # def show_currentuser_targeter_name
+  #   if (currentuser_targeter_name = targeter_.currentuser).present?
+  #     current_user_targeter_name.targeter_name
+  #   else
+  #     'まだメッセージはありません。'
+  #   end
+  # end
+
 end
