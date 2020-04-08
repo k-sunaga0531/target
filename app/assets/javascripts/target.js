3a84c5fd2c2dbc0c2e174993ae162c4b682a5262
kazuho0531 <kazuho.053187@gmail.com>
2020-04-08 18:36:05 +0900

    一旦コミット

--- app/assets/javascripts/target.js
+++ app/assets/javascripts/target.js
@@ -1,13 +1,17 @@
 
 function changeColor(idname){
   var obj = document.getElementById(idname);
-  obj.style.color = '#ffffff';            //文字色を白にする
-  obj.style.backgroundColor = 'blue';  //背景色を赤にする
+    obj.style.color = '#ffffff';            //文字色を白にする
+    obj.style.backgroundColor = 'powderblue';  //背景色を青にする
+  var obj = document.getElementById(idname);
+    obj.style.color = '#ffffff';            //文字色を白にする
+    obj.style.backgroundColor = 'powderblue';  //背景色を青にする
 }
 
-function canvasClickaka() {
-  var cvs = document.getElementById('testcanvas2');
-  var byouga = cvs.getContext('2d');
-  byouga.fillStyle = "red";
-  byouga.fillRect(50, 50, 70, 70);
-}
+
+// function canvasClickaka() {
+//   var cvs = document.getElementById('testcanvas2');
+//   var byouga = cvs.getContext('2d');
+//   byouga.fillStyle = "red";
+//   byouga.fillRect(50, 50, 70, 70);
+// }
