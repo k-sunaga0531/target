115fc7e437f14039cd7e0213744bc113326e0543
kazuho0531 <kazuho.053187@gmail.com>
2020-04-09 14:14:25 +0900

    日本語表記に統一

--- app/controllers/targeters_controller.rb
+++ app/controllers/targeters_controller.rb
@@ -4,10 +4,9 @@ class TargetersController < ApplicationController
   def index
     @targeter = Targeter.new
     @targeters = @group.targeters.includes(:user)
-    @name = Targeter.find { |targeter| targeter.user_id == current_user.id && targeter.group_id == @group.id}
+    # @name = Targeter.find { |targeter| targeter.user_id == current_user.id && targeter.group_id == @group.id}
     # name = Targeter.find { |targeter| targeter.group_id == @group.id }
     # @name = Targeter.find { |targeter| @group.user.user_id == targeter.user_id }
-
   end
 
   def create
