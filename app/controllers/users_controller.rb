84f36182195e66689271b242f54290375e2ea29c
kazuho0531 <kazuho.053187@gmail.com>
2020-04-09 12:18:34 +0900

    編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:user_name, :email)
   end
 
 end
